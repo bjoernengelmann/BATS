b38558b701743531d1cb7deb7530686e2c9e03b9
Fabian Haak <fabian@nat079024.nat.FH-Koeln.DE>
2023-10-09 18:26:01 +0200

    labeling functions updated <3

--- workspace/labeling_functions.py
+++ workspace/labeling_functions.py
@@ -499,6 +499,54 @@ def make_med_imageability_lf(imageability_threshold, label=SIMPLE):
         pre=[spacy_nlp]
     )
 
+# Fabian: frequency of past perfect aspect verbs~\cite{textevaluator}
+def num_past_perfect(x, thresh, label):
+  num_w = len([w for w in x.simp_doc if w.tag_ == "VBN"])
+  if label == SIMPLE:
+      if num_w <= thresh:
+        return label
+      else:
+        return ABSTAIN
+  else:
+    if num_w > thresh:
+      return label
+    else:
+      return ABSTAIN
+
+def make_num_past_perfect_lf(thresh, label=SIMPLE):
+
+    return LabelingFunction(
+        name=f"num_past_perfect{thresh}",
+        f=num_past_perfect,
+        resources=dict(thresh=thresh, label=label),
+        pre=[spacy_nlp]
+    )
+
+
+# Fabian: percentage of past perfect aspect verbs~\cite{textevaluator}
+def perc_past_perfect(x, thresh, label):
+  num_w = len([w for w in x.simp_doc if w.tag_ == "VBN"])/len([w for w in x.simp_doc if w.pos_ == "VERB"])
+  if label == SIMPLE:
+      if num_w <= thresh:
+        return label
+      else:
+        return ABSTAIN
+  else:
+    if num_w > thresh:
+      return label
+    else:
+      return ABSTAIN
+
+def make_perc_past_perfect_lf(thresh, label=SIMPLE):
+
+    return LabelingFunction(
+        name=f"perc_past_perfect{thresh}",
+        f=perc_past_perfect,
+        resources=dict(thresh=thresh, label=label),
+        pre=[spacy_nlp]
+    )
+
+
 # Fabian: average number of words before the main verb~\cite{textevaluator}
 def avg_num_words_before_main_verb(x, thresh, label):
   num_w = np.mean([[token.dep_ for token in sentence].index("ROOT") for sentence in x.simp_doc.sents])
@@ -1854,9 +1902,12 @@ def get_all_lfs():
   ratio_academic_word_list_complex_lfs = [make_ratio_academic_word_list_lf(thresh, label=NOT_SIMPLE) for thresh in [0.14, 0.19, 0.25]]
   avg_num_words_before_main_verb_lfs = [make_avg_num_words_before_main_verb_lf(thresh, label=SIMPLE) for thresh in [1, 2, 3, 4, 6, 8]]
   avg_num_words_before_main_verb_complex_lfs = [make_avg_num_words_before_main_verb_lf(thresh, label=NOT_SIMPLE) for thresh in [10, 12, 15]]
+  perc_past_perfect_lfs = [make_perc_past_perfect_lf(thresh, label=SIMPLE) for thresh in [0, 0.1, 0.2, 0.4, 0.6, 0.8]]
+  perc_past_perfect_complex_lfs = [make_perc_past_perfect_lf(thresh, label=NOT_SIMPLE) for thresh in [0.6, 0.8, 1]]
+  num_past_perfect_lfs = [make_num_past_perfect_lf(thresh, label=SIMPLE) for thresh in [0, 1, 2, 3, 4]]
+  num_past_perfect_complex_lfs = [make_num_past_perfect_lf(thresh, label=NOT_SIMPLE) for thresh in [5, 6, 7, 8, 12, 15]]
 
   
-
 
 
 
@@ -1877,7 +1928,8 @@ def get_all_lfs():
             entity_token_ratio_paragraph_lfs_complex + unique_entities_text_lfs_complex + average_entities_sentence_lfs_complex + average_entities_paragraph_lfs_complex +\
             unique_entity_total_entity_ratio_text_lfs_complex + unique_entity_total_entity_ratio_sentence_lfs_complex+ unique_entity_total_entity_ratio_paragraph_lfs_complex +\
             no_relative_clauses_lfs + no_relative_sub_clauses_lfs + few_anaphors_lfs + avarage_distance_appearance_same_entities_paragraph_lfs + \
-            avg_num_words_before_main_verb_lfs + avg_num_words_before_main_verb_complex_lfs
+            avg_num_words_before_main_verb_lfs + avg_num_words_before_main_verb_complex_lfs +perc_past_perfect_lfs + perc_past_perfect_complex_lfs +\
+            num_past_perfect_lfs + num_past_perfect_complex_lfs
 
   return all_lfs
 
