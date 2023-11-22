1d5b059b354d4e2d7300b83d0bfe6d5248d269ed
bjoernengelmann <bjoern.engelmann@tu-dortmund.de>
2023-11-22 12:13:25 +0100

    added functionality for specifying ta and d

--- workspace/bats_label_model.py
+++ workspace/bats_label_model.py
@@ -4,14 +4,99 @@ import numpy as np
 from pruning_lfs import prune_lfs
 from snorkel.labeling import PandasLFApplier
 import pandas as pd
+import glob
 
 class BatsModel:
-    def __init__(self, ds_name):
-        simp_path = f"/workspace/datasets/ds_labels/{ds_name}_simp_labels.pkl"        
-        src_path = f"/workspace/datasets/ds_labels/{ds_name}_src_labels.pkl"  
 
-        self.simp_labels = pickle.load(open(simp_path, "rb"))
-        self.src_labels = pickle.load(open(src_path, "rb"))
+    @classmethod
+    def get_avail_ds(cls):
+        ds = set([ds.split("/")[-1].split("_")[0] for ds in sorted(glob.glob("/workspace/datasets/ds_labels/*"))])
+        ds.add("eval")
+        return ds
+
+    @classmethod
+    def get_avail_tas(cls):
+        ta_map = pickle.load(open("/workspace/datasets/ta_map.pkl", "rb"))
+        tas = []
+        avail_ds = cls.get_avail_ds()
+        
+        for ds in avail_ds:
+            if ds in ta_map.keys():
+                tas += ta_map[ds].split(",")
+
+        return set(tas), ta_map
+
+    @classmethod
+    def get_avail_domains(cls):
+        d_map = pickle.load(open("/workspace/datasets/domain_map.pkl", "rb"))
+        domains = []
+        avail_ds = cls.get_avail_ds()
+        
+        for ds in avail_ds:
+            if ds in d_map.keys():
+                domains += d_map[ds].split(",")
+
+        return set(domains), d_map
+
+    def __init__(self, ds_name):
+
+        avail_ta, ta_map = self.get_avail_tas()
+        avail_domains, domain_map = self.get_avail_domains()
+        avail_ds = self.get_avail_ds()
+
+        if ds_name in avail_ta:
+            #load all datasets for specified target audience
+
+            simp_labels = []
+            src_labels = []
+            
+            sel_ds = {k for k,v in ta_map.items() if ds_name in v}.intersection(avail_ds)
+            
+            for ds in sel_ds:
+                print(f"Include: {ds}")
+                simp_path = f"/workspace/datasets/ds_labels/{ds}_simp_labels.pkl"        
+                src_path = f"/workspace/datasets/ds_labels/{ds}_src_labels.pkl"
+
+                simp_labels.append(pickle.load(open(simp_path, "rb")))
+                src_labels.append(pickle.load(open(src_path, "rb")))
+
+            self.simp_labels = np.concatenate(simp_labels)
+            self.src_labels = np.concatenate(src_labels)
+
+        elif ds_name in avail_domains:
+            #load all datasets for specified domain
+
+            simp_labels = []
+            src_labels = []
+            
+            sel_ds = {k for k,v in domain_map.items() if ds_name in v}.intersection(avail_ds)
+            
+            for ds in sel_ds:
+                print(f"Include: {ds}")
+                simp_path = f"/workspace/datasets/ds_labels/{ds}_simp_labels.pkl"        
+                src_path = f"/workspace/datasets/ds_labels/{ds}_src_labels.pkl"
+
+                simp_labels.append(pickle.load(open(simp_path, "rb")))
+                src_labels.append(pickle.load(open(src_path, "rb")))
+
+            self.simp_labels = np.concatenate(simp_labels)
+            self.src_labels = np.concatenate(src_labels)
+
+        elif ds_name in avail_ds:
+            simp_path = f"/workspace/datasets/ds_labels/{ds_name}_simp_labels.pkl"        
+            src_path = f"/workspace/datasets/ds_labels/{ds_name}_src_labels.pkl"  
+
+            self.simp_labels = pickle.load(open(simp_path, "rb"))
+            self.src_labels = pickle.load(open(src_path, "rb"))
+
+        else:
+            print("Please specify a correct dataset/target audience/domain")
+            print("Following are available:")
+            print(f"Datasets: {avail_ds}")
+            print(f"Target Audience: {avail_ta}")
+            print(f"Domains: {avail_domains}")
+
+            return 
 
         self.train_model()
 
@@ -61,3 +146,5 @@ class BatsModel:
                 ns_val += weights[i]
 
         return ns_val / (s_val + ns_val)
+
+    
