b8d3e6af31cf311b66373d29cd3638f770019b00
Christin Kreutz <kreutzch@gmx.de>
2023-11-27 19:56:39 +0800

    sk dims changed to 00, 10, 01. pruning per TA, D implemented

--- workspace/pruning_lfs.py
+++ workspace/pruning_lfs.py
@@ -16,14 +16,29 @@ from snorkel.labeling import LFAnalysis
 
 import warnings
 
-
-def prune_lfs():
+#datasets = []
+def prune_lfs(CHOSEN_DS = '', TARGETAUDIENCE = None, DOMAIN = None):
     all_lfs = get_all_lfs()
-    labels_simp = pickle.load(open("/workspace/datasets/eval_simp_labels.pkl", "rb"))
-    labels_src = pickle.load(open("/workspace/datasets/eval_src_labels.pkl", "rb"))
+    datasets = []
 
-    lfa_simp = LFAnalysis(L=labels_simp, lfs=all_lfs).lf_summary()
-    lfa_src = LFAnalysis(L=labels_src, lfs=all_lfs).lf_summary()
+    if len(CHOSEN_DS) > 0:
+        datasets = [CHOSEN_DS]
+    else:
+        metadata_ds = pd.read_excel('/workspace/datasets/English_Datasets.xlsx')
+
+        for index, row in metadata_ds.iterrows():
+            print(row['Target_Audience'])
+            if TARGETAUDIENCE in row['Target_Audience'].split(', ') or DOMAIN in row['Domain'].split(', '):
+                datasets.append(row['ds_id'])
+
+    labels_simp_list = []
+    labels_src_list = []
+    for dataset in datasets:
+        labels_simp_list.append(pickle.load(open("/workspace/datasets/__all_LFs/" + dataset + "_simp_labels.pkl", "rb")))
+        labels_src_list.append(pickle.load(open("/workspace/datasets/__all_LFs/" + dataset + "_src_labels.pkl", "rb")))
+
+    lfa_simp = LFAnalysis(L=np.vstack(labels_simp_list), lfs=all_lfs).lf_summary()
+    lfa_src = LFAnalysis(L=np.vstack(labels_src_list), lfs=all_lfs).lf_summary()
     
     df_simp = pd.DataFrame(lfa_simp)
     df_src = pd.DataFrame(lfa_src)  
@@ -60,28 +75,28 @@ def prune_lfs():
         if row['precision'] > 0:
             if row['total_coverage'] >= 0.05:
                 if row['precision'] >= 0.7:
-                    decision.append('JA')
+                    decision.append('YES')
                 else: 
                     if row['precision'] >= 0.5 and row['norm_dist'] >= 0.05:
-                        decision.append('JA')
+                        decision.append('YES')
                     else:
-                        decision.append('NEIN')
+                        decision.append('NO')
             else:
                 if row['precision'] > 0.5 and row['distance'] >= 0.005 and row['total_coverage'] >= 0.02:
-                    decision.append('JA')
+                    decision.append('YES')
                 else:
-                    decision.append('NEIN')
+                    decision.append('NO')
         else:
-            decision.append('NEIN')
+            decision.append('NO')
 
     df_md['decision'] = decision
 
-    df_md.to_excel("/workspace/datasets/merged_label_data.xlsx")  
+    df_md.to_excel("/workspace/datasets/" + str(datasets) + "_" + TARGETAUDIENCE + "_" + DOMAIN + "_label_data.xlsx")  
 
     keepLFs = []
 
     for index, row in df_md.iterrows():
-        if row['decision'] == 'JA':
+        if row['decision'] == 'YES':
             keepLFs.append(row['name'])
 
     selected_lfs = [a for a in all_lfs if a.name in keepLFs]
